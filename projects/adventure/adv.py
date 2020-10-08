0843bca1e0e7f61905ab32637a09ec2474c85a8e
Jessica <duellje@oregonstate.edu>
2020-10-08 16:22:21 -0400

    passing map 1 and map 2

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -23,8 +23,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt" #Main maze once done
@@ -60,15 +60,26 @@ opposite_directions = {
     'e': 'w'
 }
 
+#keep track of current path in case of dead end
+go_back_path = []
+
+#gets direction of an unexplored exit 
 def getUnexploredExitDir():
     for direc in traversal_graph[player.current_room.id]:
         if traversal_graph[player.current_room.id][direc] == '?':
             return direc
 
+steps = 0
+
 #go until the traversal graph has the same length as the room graph 
 while len(traversal_graph) < len(room_graph):   
-    print('path', traversal_path)
-    print('graph', traversal_graph) 
+    print(f'Step: {steps}')
+    print('traversal path', traversal_path) 
+    print('current room', player.current_room.id)
+    print('go back path', go_back_path)
+    print('prev room', prev_room)
+    print('prev dir', prev_dir)
+    
     #get current room exits
     exits = player.current_room.get_exits()
     
@@ -85,26 +96,40 @@ while len(traversal_graph) < len(room_graph):
         break
     
     #check if coming from previous room 
-    if prev_room:
+    if prev_room is not None:
         #updates the current and prev room with the corresponding directions
         traversal_graph[prev_room][prev_dir] = player.current_room.id
         traversal_graph[player.current_room.id][opposite_directions[prev_dir]] = prev_room
-    
+        
+    print('graph', traversal_graph)
     #get the first unexplored room using 
     direc = getUnexploredExitDir()
-    print(direc)
 
     if direc:
         traversal_path.append(direc)
+        go_back_path.append(opposite_directions[direc])
         prev_room = player.current_room.id
         prev_dir = direc
         player.travel(direc)
         traversal_graph[prev_room][direc] = player.current_room.id
+        
+        steps += 1
     else:
         #if didn't find any ? in current room exits go back the way we came until we find ? 
-        old_dir = traversal_path[-1]
-        player.travel(old_dir)
+        while direc is None:
+            #get last direction in path
+            old_dir = go_back_path.pop()
+            traversal_path.append(old_dir)
+            player.travel(old_dir)
+            direc = getUnexploredExitDir()
+            steps += 1
+        #resets previous trackers 
+        go_back_path = []
+        prev_room = None
+        prev_dir = None
+        
     
+    print("---------------------------")
 print('path', traversal_path)
 print('graph', traversal_graph)
 
